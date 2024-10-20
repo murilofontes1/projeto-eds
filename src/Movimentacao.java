321c22a86644b8ebda054e883570b45cc4b458b4
murilofontes1 <murilofontes1@gmail.com>
2024-10-20 10:21:54 -0300

    added Date and db insert method

--- src/Movimentacao.java
+++ src/Movimentacao.java
@@ -1,9 +1,15 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+
 public class Movimentacao {
     private String tipo; // "receita" ou "despesa"
     private float valor;
-    private String data;
+    private Date data;
 
-    public Movimentacao(String tipo, float valor, String data) {
+    public Movimentacao(String tipo, float valor, Date data) {
         this.tipo = tipo;
         this.valor = valor;
         this.data = data;
@@ -17,7 +23,21 @@ public class Movimentacao {
         return valor;
     }
 
-    public String getData() {
+    public Date getData() {
         return data;
+    }
+
+    // Método para adicionar uma movimentação no banco de dados
+    public void adicionarMovimentacao(Connection conn) throws SQLException {
+        String sql = "INSERT INTO movimentacao (tipo, valor, data) VALUES (?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, this.tipo);
+            pstmt.setFloat(2, this.valor);
+            
+            // Converter java.util.Date para java.sql.Date antes de inserir
+            pstmt.setDate(3, new java.sql.Date(this.data.getTime())); // Usando java.sql.Date para a inserção
+            pstmt.executeUpdate();
+            System.out.println("Movimentação adicionada com sucesso!");
+        }
     }
 }
