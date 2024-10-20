ad0c3f3d53970da4a51cebcad10400ae310db483
murilofontes1 <murilofontes1@gmail.com>
2024-10-20 11:43:07 -0300

    added 'saldo' attribute for Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,10 +2,12 @@ public class Usuario {
     private String nome;
     private String cpf;
     private boolean estadoLogin;
+    private float saldo;
 
     public Usuario(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
+        this.saldo = 0.0f;
         this.estadoLogin = false;
     }
 
@@ -19,5 +21,13 @@ public class Usuario {
 
     public boolean isLoggedIn() {
         return this.estadoLogin;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
     }
 }
